55c13df80b638a70e5c9f8c368292cae4f640623
Michael Joseph <michaeljoseph@gmail.com>
2013-09-26 08:11:11 -0400

    s/data-science-toolbox/datalogy/g

--- datalogy/__init__.py
+++ datalogy/__init__.py
@@ -1,5 +1,5 @@
-"""A collection of command-line tools that facilitate the obtaining, scrubbing
+"""A collection of python tools that facilitate the obtaining, scrubbing
 and exploring of data."""
 
 __version__ = '0.0.1'
-__url__ = 'https://github.com/jeroenjanssens/data-science-toolbox'
+__url__ = 'https://github.com/michaeljoseph/datalogy'

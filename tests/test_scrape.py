2d0d8be435ecec9fc6ea728201a32524e56b5532
Michael Joseph <michaeljoseph+github@gmail.com>
2013-10-06 12:59:13 +0200

    Remove extraneous cli args

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -6,21 +6,14 @@ from datalogy.scrape import scrape
 class ScrapeTestCase(TestCase):
 
     def test_scrape(self):
-        output = """
+        html = """
         <table class="wikitable"><tr><td><b>
         <a href="/wiki/Afghanistan" title="Afghanistan">Afghanistan</a></b>
         </td></tr></table>
         """
-        html, expression, text, body, delimiter = (
-            output,
-            'table.wikitable > tr > td > b > a',
-            False,
-            False,
-            None
-        )
 
         self.assertEqual(
             ('<a href="/wiki/Afghanistan" '
              'title="Afghanistan">Afghanistan</a>\n'),
-            scrape(html, expression, text, body, delimiter)[0]
+            scrape(html, 'table.wikitable > tr > td > b > a')[0]
         )
